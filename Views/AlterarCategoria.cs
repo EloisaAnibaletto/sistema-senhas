40ac7e3846f56f33ba30eed6919548a5a2589e4f
ELOISA ANIBALETTO <eloisa.anibaletto@alunos.sc.senac.br>
2022-05-25 22:12:30 -0300

    ModificacaoTelaAlterarCategoria

---
+++ Views/AlterarCategoria.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Drawing;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading;
+using Views.Lib;
+using Controllers;
+
+namespace Views
+{
+    public class AlterarCategoria : BaseForm
+    {
+        CategoriaCrud parent;
+        FieldForm fieldNome;
+        FieldForm fieldDescricao;
+		ButtonForm btnConfirmar;
+        ButtonForm btnCancelar;
+
+        public AlterarCategoria(CategoriaCrud parent) : base("AlterarCategoria",SizeScreen.Small)
+        {
+            this.parent = parent;
+            this.parent.Hide();
+
+            fieldNome = new FieldForm("Nome",20,20,260,20);
+            fieldDescricao = new FieldForm("Descricao",20,100,260,60);
+
+			btnConfirmar = new ButtonForm("Confirmar", 100, 180, this.handleConfirm);
+            btnCancelar = new ButtonForm("Cancelar", 100, 220, this.handleCancel);
+
+            this.Controls.Add(fieldNome.lblField);
+            this.Controls.Add(fieldNome.txtField);
+            this.Controls.Add(fieldDescricao.lblField);
+            this.Controls.Add(fieldDescricao.txtField);
+            this.Controls.Add(btnConfirmar);
+            this.Controls.Add(btnCancelar);
+        }
+
+        private void handleConfirm(object sender, EventArgs e)
+        {
+            /*try {
+                CategoriaController.AlterarCategoria(
+                    this.SelectedItems.Id,
+                    this.fieldNome.txtField.Text,
+                    this.fieldDescricao.txtField.Text
+                );
+                this.parent.LoadInfo();
+                this.parent.Show();
+                this.Close();
+            } catch (Exception err) {
+                MessageBox.Show(err.Message);
+            }*/
+        }
+        private void handleCancel(object sender, EventArgs e)
+        {
+            if (MessageBox.Show(" Deseja mesmo sair? ", "Mensage do sistema ", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    
+    }
+}
